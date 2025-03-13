1bb1e52b89133a3c0b3a0cff8bae1ddd5cf6fb94
W3NDO <wendonyang@gmail.com>
2025-03-14 01:40:27 +0300

    feat(main) - removed unnecessary channel

--- app/obrc/process.rb
+++ app/obrc/process.rb
@@ -1,5 +1,15 @@
 module OBRC
     class RowsProcessor
-        
+        def read_file
+            file = File.readlines("../data/weather_stations.csv")
+            file.each do |line|
+                city, temp = line.split(";")
+                pp ">> #{[city, temp]}"
+            end
+        end
     end
 end
+
+rp = OBRC::RowsProcessor.new
+
+rp.read_file
